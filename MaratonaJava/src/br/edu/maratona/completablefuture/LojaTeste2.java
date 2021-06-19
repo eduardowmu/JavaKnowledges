0e552315a2bc708b7ac5753424868caf578c5174
eduardowmu <eduardowmu@yahoo.com.br>
2021-06-18 23:08:45 -0300

    Autenticacao

--- MaratonaJava/src/br/edu/maratona/completablefuture/LojaTeste2.java
+++ MaratonaJava/src/br/edu/maratona/completablefuture/LojaTeste2.java
@@ -2,6 +2,8 @@ package br.edu.maratona.completablefuture;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
@@ -13,10 +15,22 @@ public class LojaTeste2
 								  new Loja("Marabras"),
 								  new Loja("CEM"),
 								  new Loja("Casas Bahia"));
-	
+		
+		System.out.println(Runtime.getRuntime().availableProcessors());
 		findPrices(lojas);
 		findPrices2(lojas);
 		findPrices3(lojas);
+		/*Usando a Thread Factory pois podemos mudar como as Threads 
+		 *serão criadas*/
+		final Executor executor = Executors.newFixedThreadPool(
+				Math.min(lojas.size(), 100), r -> {
+					Thread t = new Thread(r);
+					/*As Thread daimon não impedem a execução do programa,
+					 *a não ser que paremos todas as threads não daemon.*/ 
+					t.setDaemon(true);
+					return t;
+				});
+		findPrices4(lojas, executor);
 	}
 
 	private static List<String> findPrices(List<Loja> lojas)
@@ -56,6 +70,22 @@ public class LojaTeste2
 		List<CompletableFuture<String>> colecao = lojas.stream().map(loja -> 
 			CompletableFuture.supplyAsync(() -> String.format("%s o preço é: %.2f", 
 			loja.getNome(), loja.getPreco()))).collect(Collectors.toList());
+		
+		List<String> precosFuturos = colecao.stream().map(CompletableFuture::join)
+				.collect(Collectors.toList());
+		
+		System.out.println("Tempo final: " + (System.currentTimeMillis() - start));
+		System.out.println(precosFuturos);
+		return precosFuturos;
+	}
+	
+	private static List<String> findPrices4(List<Loja> lojas, Executor executor)
+	{	System.out.println("CompletableFuture Assincrono");
+		long start = System.currentTimeMillis();
+		
+		List<CompletableFuture<String>> colecao = lojas.stream().map(loja -> 
+			CompletableFuture.supplyAsync(() -> String.format("%s o preço é: %.2f", 
+			loja.getNome(), loja.getPreco()), executor)).collect(Collectors.toList());
 		
 		List<String> precosFuturos = colecao.stream().map(CompletableFuture::join)
 				.collect(Collectors.toList());
